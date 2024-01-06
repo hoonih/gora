f1e8b80961a923b9a8cad9eaf4c1f4428f648543
uielPark <parkuiery@gmail.com>
2024-01-07 04:13:44 +0900

    🌱

---
+++ app/src/main/java/com/example/gora/ResultActivity.kt
@@ -0,0 +1,11 @@
+package com.example.gora
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ResultActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_result)
+    }
+}
